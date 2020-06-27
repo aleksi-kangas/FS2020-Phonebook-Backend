eedb55a94bd2f27b8af7c4f35b540f02894ba478
Aleksi Kangas <aokang@utu.fi>
2020-06-27 14:54:42 +0300

    Frontend build

---
+++ build/precache-manifest.8580eca163ddff71ebc6b400463562a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6eeb46acd54056f0b0f619a3a8beb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8342caf3aae5a6f83fae",
+    "url": "/static/css/main.faf76b96.chunk.css"
+  },
+  {
+    "revision": "852868abd8e11101c0b0",
+    "url": "/static/js/2.1fd0c08b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fd0c08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8342caf3aae5a6f83fae",
+    "url": "/static/js/main.2c3b3d71.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
---
+++ build/precache-manifest.87bea6ed675a4ed91ea45c4772611dca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3f351d1dd60503643ea425a444688a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "527ed0ed3bab6eb2762f",
+    "url": "/static/css/main.faf76b96.chunk.css"
+  },
+  {
+    "revision": "852868abd8e11101c0b0",
+    "url": "/static/js/2.1fd0c08b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fd0c08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527ed0ed3bab6eb2762f",
+    "url": "/static/js/main.8c30ca0b.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
