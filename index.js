746de59bc5be467fcd33edfa179fe9cb6ee68977
Aleksi Kangas <aokang@utu.fi>
2020-06-29 15:15:33 +0300

    Part 3.13-3.18

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
+
+// MIDDLEWARE
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -22,87 +26,85 @@ morgan.token('data', (req, res) => {
 app.use(morgan(
     ":method :url :status :res[content-length] - :response-time ms :data"))
 
-let people = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
-]
+// ROUTES
 
 app.get('/info', (req, res) => {
-    const info = `There is information of ${people.length} people in the phonebook.`
-    const time = Date()
-
-    res.send(`<p>${info}</p><p>${time}</p>`)
+    Person.find({}).then(people => {
+        const info = `There is information of ${people.length} people in the phonebook.`
+        const time = Date()
+        res.send(`<p>${info}</p><p>${time}</p>`)
+        })
 })
 
 app.get('/api/people', (req, res) => {
-    res.json(people)
+    Person.find({}).then(people => {
+        res.json(people.map(person => person.toJSON()))
+    })
 })
 
 app.get('/api/people/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = people.find(p => p.id === id)
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person.toJSON())
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/people', (req, res) => {
-    const body = req.body
+    const person = new Person({
+        name: req.body.name,
+        number: req.body.number
+    })
 
-    // Name or number can't be missing
-    if (!body.name || !body.number ) {
-        return res.status(400).json({
-            error: 'content missing'
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson.toJSON())
         })
-    }
+        .catch(error => next(error))
+})
 
-    // Name must be unique
-    if (!people.every(p => p.name !== body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
+app.put('/api/people/:id', (req, res, next) => {
     const person = {
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * 1000)
+        number: req.body.number
     }
-
-    people = people.concat(person)
-    res.json(person)
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/people/:id', (req, res) => {
-    const id = Number(req.params.id)
-    people = people.filter(p => p.id !== id)
-
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
+// Unknown endpoint
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+// Error handler middleware
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformed id'})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
