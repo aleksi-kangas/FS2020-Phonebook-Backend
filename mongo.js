2546c88d2840e7a656d44f169c73e3ca216194d8
Aleksi Kangas <aokang@utu.fi>
2020-06-29 12:37:53 +0300

    Part 3.12

---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://Fullstack:${password}@fullstack-o5fqw.mongodb.net/people?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name === undefined || number === undefined) {
+    Person
+        .find({})
+        .then(people => {
+            console.log("Phonebook:")
+            people.map(person => console.log(person.name, person.number))
+            mongoose.connection.close()
+        })
+} else {
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
